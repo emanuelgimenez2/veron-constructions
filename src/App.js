98ae9a187cc610a38d36b860073895613139a262
daniboyka <danielcaz@gmail.com>
2023-06-27 22:35:31 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';import React from 'react';import HomePage from './container/HomePage';
+import Footer from './container/Footer.js';
+
 
 function App() {
   return (
     <div className="App">
      <HomePage />
+     <Footer/>
     </div>
   );
 }
