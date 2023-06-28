c4d893d0bde997426dcf83a5255457a65eaee2fe
emanuelgimenez2 <emanuelgimenez2@gmail.com>
2023-06-28 16:36:26 -0300

    componentes navbar, sociales sujeto aprobacion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';import React from 'react';import HomePage from './container/HomePage';
+
+import './App.css';import React from 'react';import HomePage from './container/HomePage';import footer from './container/sociales';import IndexPage from './container/IndexPage';import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     <HomePage />
+     <IndexPage />
     </div>
   );
 }
